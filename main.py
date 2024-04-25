38ff7835edc621ed9d74866e56edd13e241e82cd
Rakhat <81379577+Rakhat228@users.noreply.github.com>
2024-04-26 00:55:57 +0500

    Update main.py

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ def classify():
 
 dataset = st.file_uploader("UPLOAD TEXT FILE", type = ['csv'])
 if dataset is not None:
-    df_test = pd.read_csv(dataset, engine='python', header=None, delimiter = ',', header = None)
+    df_test = pd.read_csv(dataset, engine='python', header=None, delimiter = ',', skiprows = 0)
     st.write(df_test)
 
 st.button('Classify', on_click=classify, disabled=False)
