8a13c9e71d3d2a79c63636ab510ee7d287638eaf
Rakhat <81379577+Rakhat228@users.noreply.github.com>
2024-04-25 23:26:36 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import pickle
+import numpy as np
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score, log_loss
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+import streamlit as st
+
+
+
